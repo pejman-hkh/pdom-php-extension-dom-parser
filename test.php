f0f85b70d1ee52614421f37b5a4f386a379818cd
pejman <pejman.hkh@gmail.com>
2024-02-26 21:48:24 +0330

    change test file

--- test.php
+++ test.php
@@ -39,8 +39,18 @@ $s = microtime(1);
 $dom = pdom( $data );
 //print_r( $dom );
 
-//file_put_contents( 'a.json', json_encode( $dom ) );
-$find = findAttr( ['class' => 'ipc-image'], $dom['document'] );
-print_r( $find[0]['attrs']['src']."\n" );
+file_put_contents( 'a.json', json_encode( $dom ) );
+// $find = findAttr( ['class' => 'ipc-image'], $dom['document'] );
+// print_r( $find[0]['attrs']['src']."\n" );
 
 echo (microtime(1) - $s) * 1000;
+
+echo "\n";
+
+$s = microtime(1);
+
+$doc = new DOMDocument();
+@$doc->loadHTML($data);
+echo (microtime(1) - $s) * 1000;
+
+echo "\n";
